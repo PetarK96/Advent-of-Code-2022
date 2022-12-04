8874694d468bedd92968df87ab87bdeb11617ee7
PetarK96 <PetarK1996@gmail.com>
2022-12-04 05:29:24 +0000

    Add files via upload

---
+++ advent_2022_04_2.py
@@ -0,0 +1,21 @@
+f = open("advent_2022_04_input.txt")
+#f = open("test_input.txt")
+
+lines = [line.strip() for line in f.readlines()]
+
+f.close()
+
+count = 0
+
+for line in lines:
+    p1, p2 = line.split(',')
+    p1 = p1.split('-')
+    p2 = p2.split('-')
+
+    set1 = set(range(int(p1[0]), int(p1[1]) + 1))
+    set2 = set(range(int(p2[0]), int(p2[1]) + 1))
+
+    if set1.intersection(set2):
+        count += 1
+
+print(count)
