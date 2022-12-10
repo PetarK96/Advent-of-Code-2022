7f24bda4c933705655e6477bb20a4680a3e9c70a
PetarK96 <PetarK1996@gmail.com>
2022-12-10 05:52:13 +0000

    Update advent_2022_10_2.py

--- advent_2022_10_2.py
+++ advent_2022_10_2.py
@@ -8,33 +8,21 @@ f.close()
 cycle = 0
 X = 1
 
-strength = 0
-
 current_row = ['#'] * 40
-
-def check_cycle():
-    global strength, cycle, current_row
-    if cycle == 40:
-        strength += (cycle * X)
-        print(current_row)
-        current_row = ['#'] * 40
-        cycle = 0
-
-def place_value():
-    value = '#' if cycle - 1 in (X - 1, X, X + 1) else '.'
-    current_row[cycle - 1] = value
+rows = []
 
 for line in lines:
-    if line == "noop":
+    for _ in range(1 if line == "noop" else 2):
         cycle += 1
-        place_value()
-        check_cycle()
-    else:
-        _, value = line.split(" ")
-        for i in range(2):
-            cycle += 1
-            place_value()
-            check_cycle()
-        X += int(value)
+        current_row[cycle - 1] = '#' if cycle - 1 in (X - 1, X, X + 1) else '.'
 
-print(strength)
+        if cycle == 40:
+            rows.append(current_row)
+            current_row = ['#'] * 40
+            cycle = 0
+
+    if line != "noop":
+        X += int(line.split(" ")[1])
+
+for row in rows:
+    print(" ".join(row))
