013c1c58d9e0c6fb994ec6590482e07a5af48083
PetarK96 <PetarK1996@gmail.com>
2022-12-12 06:51:13 +0000

    Add files via upload

---
+++ advent_2022_12_1.py
@@ -0,0 +1,39 @@
+f = open("advent_2022_12_input.txt")
+#f = open("test_input.txt")
+
+lines = [[tree for tree in line.strip()] for line in f.readlines()]
+
+f.close()
+
+GRID_HEIGHT = len(lines)
+GRID_WIDTH = len(lines[0])
+
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+for y in range(GRID_HEIGHT):
+    for x in range(GRID_WIDTH):
+        if lines[y][x] == 'S':
+            start_position = (x, y, 0)
+            lines[y][x] = 'a'
+        elif lines[y][x] == 'E':
+            end_position = (x, y)
+            lines[y][x] = 'z'
+
+frontier = [start_position]
+visited = set()
+
+while frontier:
+    x, y, steps = frontier.pop(0)
+    if (x, y) in visited:
+        continue
+    visited.add((x, y))
+    for dir in dirs:
+        new_x, new_y = x + dir[0], y + dir[1]
+        if new_x >= 0 and new_y >= 0 and new_x < GRID_WIDTH and new_y < GRID_HEIGHT and (new_x, new_y) not in visited:
+            if ord(lines[new_y][new_x]) - ord(lines[y][x]) <= 1:
+                if (new_x, new_y) == end_position:
+                    print(steps + 1)
+                    frontier = []
+                    continue
+                else:
+                    frontier.append((new_x, new_y, steps + 1))
