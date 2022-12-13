377f78137eea60e589a296cb8bbd4752be136817
PetarK96 <PetarK1996@gmail.com>
2022-12-13 06:24:43 +0000

    Add files via upload

---
+++ advent_2022_13_1.py
@@ -0,0 +1,76 @@
+f = open("advent_2022_13_input.txt")
+#f = open("test_input.txt")
+
+lines = [line.strip() for line in f.readlines()]
+
+f.close()
+
+lines.append("")
+
+right_list = []
+
+def parse_list(text):
+    output = []
+    text = text[1:-1] + ','
+    if text == ",":
+        return []
+    start = 0
+    open_count = 0
+    for end, char in enumerate(text):
+        if char == '[':
+            open_count += 1
+        elif char == ']':
+            open_count -= 1
+        elif char == ',' and open_count == 0:
+            subtext = text[start:end]
+            open_count = 0
+            start = end + 1
+            if subtext[0] == '[':
+                output.append(parse_list(subtext))
+            else:
+                output.append(int(subtext))
+
+    return output
+
+def is_right_order(left, right):
+    checks = 0
+    for pair in zip(left, right):
+        checks += 1
+        #print(pair)
+        if isinstance(pair[0], int) and isinstance(pair[1], int):
+            if pair[0] < pair[1]:
+                return 1
+            if pair[0] > pair[1]:
+                return 0
+        else:
+            left_list, right_list = pair[0], pair[1]
+            if not(isinstance(pair[0], list)):
+                left_list = [pair[0]]
+            if not(isinstance(pair[1], list)):
+                right_list = [pair[1]]
+
+            check = is_right_order(left_list, right_list)
+            if check == 1:
+                return 1
+            if check == 0:
+                return 0
+
+    if checks < len(left):
+        return 0
+    elif checks < len(right):
+        return 1
+    else:
+        return 2
+
+
+for index, line in enumerate(lines):
+    if line == "":
+        left, right = parse_list(lines[index - 2]), parse_list(lines[index - 1])
+        pair = (index + 1) // 3
+        #print(left)
+        #print(right)
+        if is_right_order(left, right) == 1:
+            #print("Right")
+            right_list.append(pair)
+
+print(sum(right_list))
---
+++ advent_2022_13_2.py
@@ -0,0 +1,86 @@
+f = open("advent_2022_13_input.txt")
+#f = open("test_input.txt")
+
+lines = [line.strip() for line in f.readlines()]
+
+f.close()
+
+right_list = []
+
+def parse_list(text):
+    output = []
+    text = text[1:-1] + ','
+    if text == ",":
+        return []
+    start = 0
+    open_count = 0
+    for end, char in enumerate(text):
+        if char == '[':
+            open_count += 1
+        elif char == ']':
+            open_count -= 1
+        elif char == ',' and open_count == 0:
+            subtext = text[start:end]
+            open_count = 0
+            start = end + 1
+            if subtext[0] == '[':
+                output.append(parse_list(subtext))
+            else:
+                output.append(int(subtext))
+
+    return output
+
+def is_right_order(left, right):
+    checks = 0
+    for pair in zip(left, right):
+        checks += 1
+        #print(pair)
+        if isinstance(pair[0], int) and isinstance(pair[1], int):
+            if pair[0] < pair[1]:
+                return 1
+            if pair[0] > pair[1]:
+                return 0
+        else:
+            left_list, right_list = pair[0], pair[1]
+            if not(isinstance(pair[0], list)):
+                left_list = [pair[0]]
+            if not(isinstance(pair[1], list)):
+                right_list = [pair[1]]
+
+            check = is_right_order(left_list, right_list)
+            if check == 1:
+                return 1
+            if check == 0:
+                return 0
+
+    if checks < len(left):
+        return 0
+    elif checks < len(right):
+        return 1
+    else:
+        return 2
+
+packets = [parse_list(lines[0])]
+
+lines.append("")
+
+for line in lines[1:]:
+    found = False
+    if line != "":
+        line = parse_list(line)
+        for index, packet in enumerate(packets):
+            if is_right_order(line, packet) == 1:
+                packets.insert(index, line)
+                found = True
+                break
+        if not found:
+            packets.append(line)
+
+line1 = parse_list("[[2]]")
+line2 = parse_list("[[6]]")
+for line in (line1, line2):
+    for index, packet in enumerate(packets):
+        if is_right_order(line, packet) == 1:
+            packets.insert(index, line)
+            print(index)
+            break
